be67ed715d9ce6ec6f9961bb69b0ffa09e064ba6
anjansh <anjan.shakya@metropolia.fi>
2021-02-22 10:41:58 +0200

    paper installed

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
@@ -6,7 +5,6 @@ const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 };
